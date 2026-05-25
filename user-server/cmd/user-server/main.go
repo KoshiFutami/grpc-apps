b97474ee837e1fedfdc2f9b47ff0c00389b8ba1d
TLB_KoshiFutami <futamikoshi@team-lab.com>
2026-05-25 19:10:31 +0900

    feat: Go の UserService gRPC サーバを実装

--- user-server/cmd/user-server/main.go
+++ user-server/cmd/user-server/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"net"
+
+	userv1 "github.com/koshifutami/grpc-apps/user-server/gen/user/v1"
+	"google.golang.org/grpc"
+)
+
+type UserService struct {
+	userv1.UnimplementedUserServiceServer
+}
+
+func (s *UserService) GetUser(
+	ctx context.Context,
+	req *userv1.GetUserRequest,
+) (*userv1.GetUserResponse, error) {
+	return &userv1.GetUserResponse{
+		User: &userv1.User{
+			UserId: req.GetUserId(),
+			Name:   "Koshi Futami",
+			Email:  "koshi@example.com",
+		},
+	}, nil
+}
 
 func main() {
-	log.Println("TODO: start the Go gRPC user-server here")
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+
+	userv1.RegisterUserServiceServer(server, &UserService{})
+
+	log.Println("user server listening on :50051")
+
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
